88abc779f372d90570c038c6455a25b9c04a0604
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2020-09-16 15:29:54 -0400

    Added header, footer, and sidebar functions & design.

--- src/landingPage/Body.js
+++ src/landingPage/Body.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./Body.css";
+import Header from "./Header";
 
-function Body() {
+function Body({spotify}) {
     return (
         <div className="body">
-            <h1>I am the body</h1>
+            <Header spotify={spotify} />
+        </div>
+        <div className="body_info">
+            <img 
+                src="" 
+                alt="" 
+            />
+            <div className="body_infoText">
+                
+            </div>
         </div>
     )
 }
--- src/landingPage/Footer.js
+++ src/landingPage/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import "./Footer.css";
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>I am the footer</h1>
+            <div className="footer_left">
+                <img 
+                    className="footer_albumLogo" 
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Thank_U%2C_Next_album_cover.png/220px-Thank_U%2C_Next_album_cover.png" 
+                    alt="" 
+                />
+                <div className="footer_songInfo">
+                    <h4>Thank U, Next</h4>
+                    <p>Ariana Grande</p>
+                </div>
+            </div>
+            <div className="footer_center">
+                <ShuffleIcon className="footer_green" />
+                <SkipPreviousIcon className="footer_icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+                <SkipNextIcon className="footer_icon" />
+                <RepeatIcon className="footer_green" />
+            </div>
+            <div className="footer_right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
