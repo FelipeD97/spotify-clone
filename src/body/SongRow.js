e0d387b1c20a6a06a2d4ded49b4913ee9bc0c067
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2020-09-17 17:14:45 -0400

    Added song & album listing. Added CSS to body pages.

--- src/body/SongRow.js
+++ src/body/SongRow.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./SongRow.css";
 
 function SongRow({ track }) {
     return (
-        <div>
-            <p>{track.name}</p>
+        <div className="songRow">
+            <img 
+                className="songRow_album"
+                src={track.album.images[0].url} 
+                alt="album cover" 
+            />
+            <div className="songRow_info">
+                <h1>{track.name}</h1>
+                <p>
+                    {track.artists.map((artists) => artists.name).join(", ")}
+                    {track.album.name}
+                </p>
+            </div>
+            
         </div>
     )
 }
